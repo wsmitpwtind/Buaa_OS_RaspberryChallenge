d4728076f2548a17d72cdf06d66b4dcb8951f5fb
张柯 <19376075@buaa.edu.cn>
2022-06-26 13:06:15 +0800

    wqw

--- lab2/init/main.c
+++ lab2/init/main.c
@@ -1,15 +1,13 @@
 #include <printf.h>
-#include <drivers/include/uart.h>
+#include <pmap.h>
+#include <uart.h>
 
 void main() {
-    uart_init();
-    printf("System started!\n");
-    printf("This is lab1 for kezhang, xingyang huang, zichuan zheng\n");
-    int test = 3;
-    printf("Bock1<>\n %d %d",test ,test);
+    printf("mmu started!\n");
+    page_init();
+    printf("page_init ok!\n");
     while (1) {
-        char c = uart_getc();
-	printf("%c",c);
+        char c = uart_getc_kern();
+        uart_send_kern(c);
     }
 }
-
